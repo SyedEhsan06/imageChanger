10d3661ecc9e981aa83d9e65c63ff4c807066f0f
Ehsan <ctech1699@gmail.com>
2023-11-18 02:08:15 +0530

    Done

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 let imgContainer = document.querySelector('.imgContainer');
 let textContainer = document.querySelector('.textContainer');
-let progress = document.querySelector('.line');
+let progress = document.querySelectorAll('.line');
 let text = [
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Error velit unde tempora ea necessitatibus nostrum nobis accusamus inventore, nemo, natus laboriosam eligendi modi enim voluptatum magnam eveniet pariatur, minus saepe placeat harum aspernatur. Ipsum sunt accusantium sapiente, maiores dignissimos fuga!",
   "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur consequatur adipisci illo dolores esse nam culpa recusandae. Corporis nam sed asperiores architecto repellat iste sequi rem, repudiandae natus eaque animi, ratione veniam ullam voluptatum saepe culpa facere tenetur nemo quia.",
   "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officia facere ut aliquid doloremque voluptatibus doloribus voluptatem quis minus facilis earum nesciunt beatae inventore quam minima quo molestiae dolor perspiciatis nostrum nam, repudiandae, itaque veritatis quod. Delectus temporibus hic repellendus aliquam."
 ];
 let images = ["1.jpg", "2.jpeg", "y1.jpg"];
-let elem = document.getElementsByClassName("e1");
+let elem = document.querySelectorAll(".e1");
+let ei1 = document.getElementById("ei1")
 let indexOfimg = 0;
 let height = 0;
 var time = 0;
@@ -18,17 +19,6 @@ function changeImage() {
     indexOfimg = 0;
   }
 
-  elem[0].style.marginTop = `${indexOfimg * 5}vh`;
-  if (indexOfimg==0) {
-  elem[0].style.height = `15%`;
-  }
-  else if(indexOfimg==1){
-    elem[0].style.height = `16%`;
-  }
-  else{
-  elem[0].style.height = `17%`;
-
-  }
   imgContainer.firstElementChild.attributes.src.value = images[indexOfimg];
   imgContainer.firstElementChild.attributes.style.value = `animation: fade 1s ease-in-out;opacity: 1;`;
   let previousIndex = indexOfimg - 1 < 0 ? images.length - 1 : indexOfimg - 1;
@@ -43,7 +33,10 @@ var prointerval = setInterval(() => {
   if (height == 100) {
     height = 0;
   }
-  progress.style.height = `${height}%`;
+  progress.forEach((el)=>el.style.height="0%")
+  elem.forEach((elem)=>elem.style.opacity=0)
+  elem[indexOfimg].style.opacity=1
+  progress[indexOfimg].style.height = `${height}%`;
   height = height + 2;
 }, 50);
 
@@ -52,8 +45,8 @@ textContainer.addEventListener('click', (e) => {
     let index = e.target.parentElement.children[1].attributes.id.value;
     indexOfimg = index;
   
-    elem[0].style.marginTop = `${(index-1) * 5}vh`;
-    progress.style.height = `100%`;
+    progress.forEach((el)=>el.style.height="0%")
+    progress[indexOfimg-1].style.height = `100%`;
     imgContainer.firstElementChild.attributes.src.value = images[index - 1];
     Array.from(textContainer.children).forEach((child) => {
       child.children[1].innerHTML = "";
